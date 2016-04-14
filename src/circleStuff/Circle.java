70561640d6418897c88360ed0a4a81b0f9e2aa6c
Alex Marks <marks.alexanderj@gmail.com>
2016-04-13 23:18:25 -0400

    Comments added.  User Validation added.

--- src/circleStuff/Circle.java
+++ src/circleStuff/Circle.java
@@ -7,35 +7,50 @@ public class Circle {
 
 	double radius;
 
+	public Circle(double radius) {
+		// initialize attributes
+		
+		setRadius(radius);
+	}
+	
 	public double getRadius() {
+		// returns value of radius variable
 		return radius;
 	}
 
 	public void setRadius(double radius) {
+		// sets value of radius 
+		
 		this.radius = radius;
 	}
 
-	public Circle(double radius) {
-		setRadius(radius);
-	}
-
 	public double getCircumference() {
+		// calculate and return circumference of circle.
+		
 		return Math.PI * (2 * getRadius());
 	}
 
 	public String getFormattedCircumference() {
+		// return formatted value of circumference
+		
 		return formatNumber(getCircumference());
 	}
 
 	public double getArea() {
+		// calculate and return area of circle
+		
 		return Math.PI * (getRadius() * getRadius());
 	}
 
 	public String getFormattedArea() {
+		// return formatted value of circumference
+		
 		return formatNumber(getArea());
 	}
 
 	private String formatNumber(double x) {
+		// format value to two decimal points
+		
 		NumberFormat formatter = new DecimalFormat("#.00");
 		return formatter.format(x);
 
--- src/circleStuff/CircleApp.java
+++ src/circleStuff/CircleApp.java
@@ -3,37 +3,80 @@ package circleStuff;
 import java.util.Scanner;
 
 public class CircleApp {
+
 	static Scanner input = new Scanner(System.in);
+	static boolean userConsent = true;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		double radius = getInput();
+		double radius = 0;
+		int counter = 0;
+		
+		System.out.println("Welcome to the Circle Tester.");
 
-		Circle c1 = new Circle(radius);
+		while (userConsent) {
+			radius = getInput();
+			Circle c1 = new Circle(radius);
+			printStuff(c1);
+			counter++;
+			userConsent = getUserConsent();
 
-		System.out.println(c1.getFormattedCircumference());
-		System.out.println(c1.getFormattedArea());
+		}
+
+		System.out.println("You tested " + counter + " circles.  Bye!");
 		input.close();
+
+	}
+
+	public static void printStuff(Circle c1) {
+		// print stuff
+
+		System.out.println("Circumference is: " + c1.getFormattedCircumference());
+		System.out.println("Area is: " + c1.getFormattedArea());
+	}
+
+	public static boolean getUserConsent() {
+		// ask user if they want to run the program again
+
+		String c;
+		boolean consent = true;
+		
+		while (true) {
+			System.out.println("Would you like to test another circle? (y/n): ");
+			c = input.nextLine();
+			if (c.equalsIgnoreCase("y")) {
+				consent = true;
+				break;
+			} else if (c.equalsIgnoreCase("n")) {
+				consent = false;
+				break;
+			} else {
+				System.out.println("Please enter \"y\" or \"n\".");
+				continue;
+			}
+		}
+		return consent;
 	}
 
 	public static double getInput() {
-		
+		// return radius of circle from console user input.
+
 		double r = 0;
-		
+
 		while (true) {
 			try {
-				System.out.println("Welcome to the Circle Tester.");
 				System.out.print("Enter Radius: ");
 				r = input.nextDouble();
 				input.nextLine();
 				break;
 			} catch (Exception e) {
-				System.out.println("Please enter a number");
+				System.out.println("Please enter a number, not... whatever that was.");
 				input.nextLine();
 				continue;
 			}
 		}
 		return r;
 	}
+
 }
